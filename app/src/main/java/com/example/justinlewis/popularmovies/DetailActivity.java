4bbf363e7914e8b3248f23e35205eb9f7fae5abc
Saviour_Self <Juslewis@gmail.com>
2016-03-31 20:30:34 -0400

    Adjust image add text.

--- app/src/main/java/com/example/justinlewis/popularmovies/DetailActivity.java
+++ app/src/main/java/com/example/justinlewis/popularmovies/DetailActivity.java
@@ -51,13 +51,14 @@ public class DetailActivity extends ActionBarActivity {
             //System.out.println(model.getPlot_synopsis());
             View view = inflater.inflate(R.layout.fragment_detail, container, false);
             TextView movieTitle = (TextView) view.findViewById(R.id.movie_title);
-            ImageView poster = (ImageView) view.findViewById(R.id.moviePoster);
+            ImageView imageView = (ImageView) view.findViewById(R.id.moviePoster);
+
+            //imageView.setScaleType(ImageView.ScaleType.FIT_START);
+            imageView.setAdjustViewBounds(true);
 
             movieTitle.setText(model.getTitle());
             Picasso.with(view.getContext()).load(model.getPoster_url())
-                    .into(poster);
-            movieTitle.postInvalidate();
-            System.out.println("DONE!");
+                    .into(imageView);
             return view;
             //return inflater.inflate(R.layout.fragment_detail, container, false);
         }
