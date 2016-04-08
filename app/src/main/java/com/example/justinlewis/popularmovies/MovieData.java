62cf1552ff8d4554538bbbb70d5aeace60cc40d1
Saviour_Self <Juslewis@gmail.com>
2016-04-07 21:46:24 -0400

    Parcelable.

--- app/src/main/java/com/example/justinlewis/popularmovies/MovieData.java
+++ app/src/main/java/com/example/justinlewis/popularmovies/MovieData.java
@@ -1,11 +1,14 @@
 package com.example.justinlewis.popularmovies;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import java.io.Serializable;
 
 /**
  * Created by Justin Lewis on 3/30/2016.
  */
-public class MovieData implements Serializable {
+public class MovieData implements Parcelable {
 
     private String title, release_date, poster_url, vote_average, plot_synopsis;
 
@@ -16,6 +19,20 @@ public class MovieData implements Serializable {
         this.poster_url = poster_url;
         this.vote_average = vote_average;
         this.plot_synopsis = plot_synopsis;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(title);
+        dest.writeString(release_date);
+        dest.writeString(poster_url);
+        dest.writeString(vote_average);
+        dest.writeString(plot_synopsis);
     }
 
     public String getTitle() {
