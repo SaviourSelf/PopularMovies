5453f7bacb4f8bd5f9e0c1a812b4c17238000a7a
Saviour_Self <Juslewis@gmail.com>
2016-04-17 21:11:07 -0400

    Add share button.

--- app/src/main/java/com/example/justinlewis/popularmovies/DetailActivity.java
+++ app/src/main/java/com/example/justinlewis/popularmovies/DetailActivity.java
@@ -4,19 +4,26 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.ShareActionProvider;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
 
 public class DetailActivity extends ActionBarActivity {
+
+
+    private ShareActionProvider mShareActionProvider;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,6 +41,12 @@ public class DetailActivity extends ActionBarActivity {
                     .add(R.id.container, frag)
                     .commit();
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_detail, menu);
+        return true;
     }
 
     public static class DetailActivityFragment extends Fragment {
